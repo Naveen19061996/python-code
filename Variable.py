361825caeccd33bb08bc385c03aa06980f868c99
yadav.naveen770@gmail.com <yadav.naveen770@gmail.com>
2022-09-10 18:48:11 +0530

    XOR Operation

--- Variable.py
+++ Variable.py
@@ -1,51 +1,64 @@
 # Creating  a variable
-x = 5
-y = "John"
-print(x)
-print(y)
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
 
-# overiding variable
-x = 4       # x is of type int
-x = "Sally" # x is now of type str
-print(x)
+# # overiding variable
+# x = 4       # x is of type int
+# x = "Sally" # x is now of type str
+# print(x)
 
-# Casting
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
-z = float(3)  # z will be 3.0
-print(x)
-print(y)
-print(z)
+# # Casting
+# x = str(3)    # x will be '3'
+# y = int(3)    # y will be 3
+# z = float(3)  # z will be 3.0
+# print(x)
+# print(y)
+# print(z)
 
 
-#valid Variable declaration
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR = "John"
-myvar2 = "John"
+# #valid Variable declaration
+# myvar = "John"
+# my_var = "John"
+# _my_var = "John"
+# myVar = "John"
+# MYVAR = "John"
+# myvar2 = "John"
 
-#Invalid Variable declaration
-# 2myvar = "John"
-# my-var = "John"
-# my var = "John"
+# #Invalid Variable declaration
+# # 2myvar = "John"
+# # my-var = "John"
+# # my var = "John"
 
-# Many values to multiple variable
-x , y, z = "Orange","Banana","Cherry"
-print(x)
-print(y)
-print(z) 
+# # Many values to multiple variable
+# x , y, z = "Orange","Banana","Cherry"
+# print(x)
+# print(y)
+# print(z) 
 
-# One values to multiple variable
-x = y = z = "Orange"
-print(x)
-print(y)
-print(z)
+# # One values to multiple variable
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
 
 # Unpack a Collection
-fruits = [ "Orange","Banana","Cherry"]
-x,y,z = fruits
-print(x)
-print(y)
-print(z)
+# fruits = [ "Orange","Banana","Cherry"]
+# x,y,z = fruits
+# print(x)
+# print(y)
+# print(z)
+
+
+numbers = [2,3,2,3,4]
+#print(type(numbers))
+
+## XOR Operation :- To find single occurance of integers in list 
+res = 0
+for i in numbers:
+    res ^=i
+print(res)
+
+
+
